2ac109d7351472b8c8f4286a04cef48e4b7780cb
BRUNO FERREIRA DA SILVA <bruno.fsilva72@senacsp.edu.br>
2024-02-02 17:22:13 -0300

    Terceiro Commit

--- app/Http/Controllers/homeControl.php
+++ app/Http/Controllers/homeControl.php
@@ -7,6 +7,22 @@ use Illuminate\Http\Request;
 class homeControl extends Controller
 {
     public function Home() {
+        
+
         return view('paginas.home'); //Retorna para o arquivo login.blade.php
     }//Fim do Metodo
+
+    public function store(Request $request){
+        $nomeUsuario = $request->input('nome'); //Coletando dados do formulario
+        $emailUsuario = $request->input('email');
+        $senhaUsuario = $request->input('senha');
+
+        $model = new cadastrarUsuarioModel();
+        $model->nome = $nomeUsuario;
+        $model->email = $emailUsuario;
+        $model->senha = $senhaUsuario;
+        $model->save(); //Armazenar no Banco
+
+        return redirect('/');
+    }//Fim do Metodo store
 }
